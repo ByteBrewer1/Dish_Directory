482edd5aa082fff5b585afab912972708aa7dbbf
Rahul Mistry <120237914+ByteBrewer1@users.noreply.github.com>
2023-05-19 09:20:14 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,11 @@ import { ImSpinner9 } from "react-icons/im";
 
 const Home = ({ recipes, loading, error }) => {
   return (
-    <div className='home container mx-auto py-8 flex flex-wrap gap-10 justify-center'>
+    <div className="home container mx-auto py-8 flex flex-wrap gap-10 justify-center">
       {/* If there is no loading or error or if there is no recipe  */}
       {!loading && !error && recipes.length === 0 ? (
         <div>
-          <p className='text-2xl font-semibold  text-rose-300 lg:text-4xl text-center leading-normal'>
+          <p className="text-2xl font-semibold  text-rose-300 lg:text-4xl text-center leading-normal">
             Nothing to show, please search something!
           </p>
           <FryingPan />
@@ -19,11 +19,11 @@ const Home = ({ recipes, loading, error }) => {
       {loading && (
         <p>
           {error ? (
-            <span className='text-2xl font-semibold  text-rose-300 lg:text-4xl text-center leading-normal'>
+            <span className="text-2xl font-semibold  text-rose-300 lg:text-4xl text-center leading-normal">
               {error}
             </span>
           ) : (
-            <ImSpinner9 className='animate-spin' />
+            <ImSpinner9 className="animate-spin" />
           )}
         </p>
       )}
